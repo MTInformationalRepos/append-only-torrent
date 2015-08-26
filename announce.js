8bf428e7d9e8d3bfddf3470ff5146057710c02cc
James Halliday <mail@substack.net>
2015-08-26 15:11:28 -0700

    using fixed-size-stream-splitter

--- announce.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-  [ 'udp://tracker.publicbt.com:80' ],
-  [ 'udp://tracker.openbittorrent.com:80' ],
-  [ 'udp://open.demonii.com:1337' ],
-  [ 'udp://tracker.webtorrent.io:80' ],
-  [ 'wss://tracker.webtorrent.io' ] // For WebRTC peers (see: WebTorrent.io)
-]
--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-var Writable = require('readable-stream/writable')
-var BlockStream = require('block-stream2')
-var sha1 = require('simple-sha1')
-var combine = require('stream-combiner2')
-var writeonly = require('write-only-stream')
 var through = require('through2')
 var defined = require('defined')
 var bencode = require('bencode')
-var announceList = require('./announce.js')
+var createHash = require('sha.js')
+var concat = require('concat-stream')
+var sizeStream = require('fixed-size-stream-splitter')
 
 module.exports = function (opts) {
   if (!opts) opts = {}
-  var pieceLength = defined(opts.pieceLength, 4096 * 1024)
+  if (typeof opts === 'number') opts = { size: opts }
+  var size = defined(opts.size, 4096 * 1024)
   var pieces = []
-  var announce = defined(opts.announceList, opts.trackers, announceList)
-    .map(function (x) { return Array.isArray(x) ? x : [x] })
   var size = 0
 
-  var outer = writeonly(combine(
-    new BlockStream(pieceLength, { zeroPadding: false }),
-    through.obj(function (chunk, enc, next) {
-      size += chunk.length
-      sha1(chunk, function (hash) {
-        pieces.push(hash)
-        outer.emit('chunk', chunk, hash)
-        var info = {
-          name: opts.name,
-          length: size,
-          pieces: pieces,
-          'piece length': pieceLength
-        }
-        outer.emit('info', info, sha1.sync(bencode.encode(info)))
-        next()
-      })
-    })
-  ))
+  var outer = sizeStream(size, function (stream) {
+    var pending = 2, result = { length: size }
+    stream.pipe(through(write, end))
+ 
+    var h = createHash('sha1')
+    stream.pipe(h).pipe(concat(function (hash) {
+      result.hash = hash
+      done()
+    }))
+    outer.emit('stream', stream, size)
+ 
+    function write (buf, enc, next) {
+      result.length += buf.length
+      size = result.length
+      next()
+    }
+ 
+    function done () {
+      if (--pending !== 0) return
+      var info = {
+        name: opts.name,
+        length: result.length,
+        pieces: pieces,
+        'piece length': size
+      }
+      outer.emit('info', info, sha1.sync(bencode.encode(info)))
+    }
+  })
+  if (cb) outer.on('info', cb)
   return outer
 }
