776a7b4332875accf8b7d5f0baea58763c090d6f
James Halliday <mail@substack.net>
2015-08-27 19:14:48 -0700

    it works end-to-end now

--- example/append.js
+++ example/append.js
@@ -1,18 +1,26 @@
 var append = require('../')
 var torrent = require('torrent-stream')
+var magnet = require('magnet-uri')
 
 var FStore = require('fs-chunk-store')
 var store = FStore(5, { path: '/tmp/append.store' })
+var trackers = [ 'udp://127.0.0.1:9000' ]
 
 var w = append({ size: 5, store: store })
 w.on('torrent', function (t) {
-  var engine = torrent(t, { store: store })
+  var engine = torrent(t, {
+    storage: function () { return store },
+    trackers: trackers
+  })
   engine.on('ready', function () {
     var ok = engine.torrent.pieces.every(function (piece, i) {
       return engine.bitfield.get(i)
     })
     if (!ok) console.error('missing data')
-    else console.log('magnet:?xt=urn:btih:' + infoHash.toString('hex'))
+    else console.log(magnet.encode({
+      xt: 'urn:btih:' + t.infoHash,
+      tr: trackers
+    }))
   })
   engine.listen(0)
 })
--- index.js
+++ index.js
@@ -8,9 +8,10 @@ var sizeStream = require('fixed-size-stream-splitter')
 module.exports = function (opts) {
   if (!opts) opts = {}
   if (typeof opts === 'number') opts = { size: opts }
-  var pieces = []
+  var pieces = [], hexpieces = []
   var pieceLength = defined(opts.size, 4096 * 1024)
   var size = 0
+  var store = opts.store
 
   var outer = sizeStream(pieceLength, function (stream) {
     var h = createHash('sha1')
@@ -19,29 +20,40 @@ module.exports = function (opts) {
  
     function write (buf, enc, next) {
       h.update(buf)
+      var offset = size
       size += buf.length
-      next()
+      store.put(offset, buf, function (err) { next(err) })
     }
  
     function end () {
-      pieces.push(h.digest('hex'))
+      var hash = h.digest()
+      pieces.push(hash)
+      hexpieces.push(hash.toString('hex'))
+ 
       var info = {
-        name: opts.name,
+        name: defined(opts.name, 'output'),
         length: size,
-        'piece length': pieceLength
+        'piece length': pieceLength,
+        pieces: Buffer.concat(pieces)
       }
       var infoBuffer = bencode.encode(info)
       var infoHash = createHash('sha1').update(infoBuffer).digest('hex')
       if (!info.name) info.name = infoHash
-      outer.emit('torrent', {
+      var torrent = {
         info: info,
-        pieces: pieces,
         infoHash: infoHash,
         infoBuffer: infoBuffer,
         files: [
-          { offset: 0, length: size, path: '/tmp/' + infoHash }
-        ]
-      })
+          {
+            offset: 0,
+            length: size,
+            path: '/tmp/' + infoHash 
+          }
+        ],
+        pieces: hexpieces,
+        announce: defined(opts.announce, opts.trackers, [])
+      }
+      outer.emit('torrent', torrent)
     }
   })
   return outer
